215735ff050acde09e3de76f3362bf99ce27f0e0
mAhmad-cyber <mianahmadabdullah963@gmail.com>
2026-06-22 15:07:06 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>                  // input output stream
+using namespace std;
+  
+int main(){                        // main function
+
+  int num1,num2;      //user defined int-type variables
+  
+  cout<<"Enter first number:\n";
+  cin>>num1;                    
+  cout<<"Enter second number:\n";
+  cin>>num2;
+  cout<<"Result :"<<num1+num2<<endl;
+  return 0;
+}
